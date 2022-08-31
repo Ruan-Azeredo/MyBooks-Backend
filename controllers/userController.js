f58f87bd3ca7ce6d658bbb3881001bc25988cf3b
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-31 11:45:37 -0300

    :sparkles: feat: adiciona controllers de users

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const { Router } = require('express');
+const { User } = require('../models');
+
+const router = Router();
+
+router.get('/all', async (req, res) => {
+    const users = await User.findAll();
+    return res.json(users);
+});
+
+router.post('/create', async (req, res) => {
+    const { name, email, password, photo } = req.body;
+    const newuser = await User.create({ name, email, password, photo });
+    return res.json(newuser);
+});
+
+router.get('/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    return res.json(user);
+});
+
+router.delete('/:id', async (req, res) => {
+    const user = await User.destroy({ where: { id: req.params.id } });
+    id = req.params.id;
+    return res.json(`Usuario de id = ${id} deletado`);
+});
+
+router.put('/:id', async (req, res) => {
+    const { name, email, password, photo } = req.body;
+    await User.update(
+        { name, email, password, photo }, { where: { id: req.params.id } }
+    );
+    const updateuser = await User.findByPk(req.params.id);
+    return res.json(updateuser)
+});
+
+module.exports = router;
