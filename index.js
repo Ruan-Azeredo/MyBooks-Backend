0e476d11adf276bde9d378a03b4c4d09c286d586
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-31 11:44:00 -0300

    :heavy_plus_sign: build: chama controllers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const controllers = require('./controllers')
 const cors = require('cors')
 const app = express()
 const port = 3001
 
-use.app(cors())
+app.use(cors())
+
+app.use(express.json('/users', controllers.users))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
