039381f05a70217890462e86be720fa45e70f731
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-01 19:09:57 -0300

    :sparkles: feat: adiciona body parser

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const controllers = require('./controllers')
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express()
 const port = 3001
 
+app.use(bodyParser.json());
 app.use(cors())
 
-app.use(express.json('/users', controllers.users))
+app.use('/users', controllers.users)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
