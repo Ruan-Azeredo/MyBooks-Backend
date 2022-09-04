965288600db866f5c74c1c1c72f1432583a97044
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-04 14:07:25 -0300

    :construction: feat: chama writers na index contr.

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const { request } = require('express');
 const usersController = require('./usersController');
+const writersController = require('./writersController');
 
 module.exports = {
     users: usersController,
+    writers: writersController,
 };
