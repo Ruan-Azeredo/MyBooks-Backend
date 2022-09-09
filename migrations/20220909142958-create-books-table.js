2277deb6e9f21f8920bf6fa6331179ef92260e07
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-09 16:18:29 -0300

    :construction: build: adiciona migrate de livros

---
+++ migrations/20220909142958-create-books-table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      writer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Writers', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cover: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.droptable('Books');
+  }
+};
